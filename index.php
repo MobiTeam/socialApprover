90f9b6e3d3e270cd15783cb36e946fd2a5582b6f
Vladislav <vladonxp@mail.ru>
2016-08-23 22:01:55 +0500

    adding remodal

--- index.php
+++ index.php
@@ -57,7 +57,7 @@
 				</div>
 			</div>
 		</div>
-		
+		<a href="#modal">Call the modal with data-remodal-id="modal"</a>
 		<hr>
 
 	</div>
@@ -65,7 +65,17 @@
 	<div class="footer text-center">
 		Social Approver - <?php echo date('Y') ?>
 	</div>
-
+	
+	<div class="remodal" data-remodal-id="modal">
+	  <button data-remodal-action="close" class="remodal-close"></button>
+	  <h1>Remodal</h1>
+	  <p>
+	    Responsive, lightweight, fast, synchronized with CSS animations, fully customizable modal window plugin with declarative configuration and hash tracking.
+	  </p>
+	  <br>
+	  <button data-remodal-action="cancel" class="remodal-cancel">Cancel</button>
+	  <button data-remodal-action="confirm" class="remodal-confirm">OK</button>
+	</div>
 
 	<script src="js/jquery-2.2.4.min.js"></script>
 
